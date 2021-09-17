19c2359a40e2222caac6a52bdd83092a5045471e
azimmern <azimmern@users.noreply.github.com>
2021-09-17 17:52:51 -0400

    final push

--- main.js
+++ main.js
@@ -37,6 +37,18 @@ $(document).ready(function () {
         }
     });
 
+    $('.therologia a').on('click', function (e){
+        if (this.hash !== '') {
+            e.preventDefault();
+
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+
     // fade out the menu banner after a certain scroll length
     
     $(window).scroll(function() {
@@ -46,6 +58,25 @@ $(document).ready(function () {
           $('#menu-banner').fadeIn("fast");
         }
       });
+
+      var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
 
 
 });
